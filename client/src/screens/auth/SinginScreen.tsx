c42d788e195821ecb2fc4c0affb69d8cdfc73015
Justin Albert <Jnalbert879@gmail.com>
2023-01-12 08:07:17 -0800

    added screens not done

---
+++ client/src/screens/auth/SinginScreen.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+import { View } from 'react-native';
+import styled from 'styled-components/native'
+
+const SinginScreen: FC = () => {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default SinginScreen
