923d017fbfa225c9d54dbaab00b967a9911e9a6e
Justin Albert <Jnalbert879@gmail.com>
2023-01-17 09:37:04 -0800

    removed name change files

--- client/src/screens/auth/IntroScreen.tsx
+++ client/src/screens/auth/IntroScreen.tsx
@@ -7,20 +7,20 @@ import ScreenWrapperComp from "../../shared/ScreenWrapperComp";
 const IntroScreenWrapper = styled.View`
   margin-top: 12%;
   flex: 1;
-    width: 100%;
-    height: 100%;
-    align-items: center;
+  width: 100%;
+  height: 100%;
+  align-items: center;
 `;
 
 const IntroImage = styled.Image`
   width: 100%;
-`
+`;
 
 const IntroScreen: FC = () => {
   return (
     <ScreenWrapperComp noMargin backgroundColor={Peach}>
       <IntroScreenWrapper>
-        <IntroImage source={require('/Users/justinalbert/Code_Projects/CurrentProjects/OdysseyTrading/client/assets/OdysseyAppIntro.png')} />
+        <IntroImage source={require("/Users/justinalbert/Code_Projects/CurrentProjects/OdysseyTrading/client/assets/OdysseyAppIntro.png")} />
       </IntroScreenWrapper>
     </ScreenWrapperComp>
   );
