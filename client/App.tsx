a9a5fc4cbce991f21a34c5ca1ae852da66890fe5
Justin Albert <Jnalbert879@gmail.com>
2023-01-19 10:00:35 -0800

    fixed splash screen depricated issues

--- client/App.tsx
+++ client/App.tsx
@@ -1,6 +1,13 @@
-import React, { FC, useEffect, useMemo, useReducer, useState } from "react";
+import React, {
+  FC,
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useState,
+} from "react";
 import styled from "styled-components/native";
-import { useFonts } from "expo-font";
+import { loadAsync, useFonts } from "expo-font";
 import { StyleSheet, Text, View } from "react-native";
 import {
   Grandstander_500Medium,
@@ -11,35 +18,57 @@ import { Mulish_500Medium, Mulish_700Bold } from "@expo-google-fonts/mulish";
 import { NavigationContainer } from "@react-navigation/native";
 import MainTabNavigator from "./src/navigators/main/MainTabNavigator";
 import AuthNavigator from "./src/navigators/auth/AuthNavigator";
-import AppLoading from "expo-app-loading";
+import { hideAsync, preventAutoHideAsync } from "expo-splash-screen";
 
 const AppWrapperView = styled.View`
   flex: 1;
 `;
 
-const App: FC<any> = () => {
-  let [isFontLoaded] = useFonts({
-    GrandstanderExtraBold: Grandstander_800ExtraBold,
-    GrandstanderSemiBold: Grandstander_600SemiBold,
-    GrandstanderMedium: Grandstander_500Medium,
-    MulishBold: Mulish_700Bold,
-    MulishMedium: Mulish_500Medium,
-  });
+preventAutoHideAsync();
 
-  console.log(isFontLoaded);
+const App: FC<any> = () => {
+  const [isAppReady, setIsAppReady] = useState(false);
+
+  useEffect(() => {
+    async function loadData() {
+      try {
+        // loading fonts
+        await loadAsync({
+          GrandstanderExtraBold: Grandstander_800ExtraBold,
+          GrandstanderSemiBold: Grandstander_600SemiBold,
+          GrandstanderMedium: Grandstander_500Medium,
+          MulishBold: Mulish_700Bold,
+          MulishMedium: Mulish_500Medium,
+        });
+        // do other fetches here
+        // ***************
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setIsAppReady(true);
+      }
+    }
+
+    loadData();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isAppReady) {
+      // Hides the splash screen
+      await hideAsync();
+    }
+  }, [isAppReady]);
+
+  if (!isAppReady) {
+    return null;
+  }
 
   return (
-    <>
-      {!isFontLoaded ? (
-        <AppLoading />
-      ) : (
-        <NavigationContainer>
-          <AppWrapperView>
-            <AuthNavigator />
-          </AppWrapperView>
-        </NavigationContainer>
-      )}
-    </>
+    <NavigationContainer onReady={onLayoutRootView}>
+      <AppWrapperView>
+        <AuthNavigator />
+      </AppWrapperView>
+    </NavigationContainer>
   );
 };
 
