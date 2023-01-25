6d22213aa22f19d2585855ba441063c8a316dbcb
Justin Albert <Jnalbert879@gmail.com>
2023-01-25 11:09:42 -0800

    animation not finished

--- client/firebase/FirestoreFunctions.ts
+++ client/firebase/FirestoreFunctions.ts
@@ -1,15 +1,24 @@
-import { addDoc, collection, deleteDoc, doc, onSnapshot, updateDoc } from "firebase/firestore"
-import { db } from "../config/firebase"
-import { ActiveTradeType } from "./types/ActiveTradeType"
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  onSnapshot,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "../config/firebase";
+import { _getUuid } from "../src/AppContext";
+import { ActiveTradeType } from "./types/ActiveTradeType";
 
-export const addNewAccountToDB = async () => {
-  
-}
-
+export const addNewAccountToDB = async () => {};
 
 // *** Various Trading Functions *** //
 
-export const startActiveTrade = async (userData: {uuid: string, username: string}): Promise<{tradeCode: string}> => {
+export const startActiveTrade = async (userData: {
+  uuid: string;
+  username: string;
+}): Promise<{ tradeCode: string }> => {
   // Create a new trade in db
   const tradeDocData: ActiveTradeType = {
     sendUserUuid: userData.uuid,
@@ -23,40 +32,74 @@ export const startActiveTrade = async (userData: {uuid: string, username: string
     senderConfirmed: false,
     receiverConfirmed: false,
     isCanceled: false,
-  }
+  };
   // adds doc to DB
-  const tradeDoc = await addDoc(collection(db, "active-trades"), tradeDocData)
+  const tradeDoc = await addDoc(collection(db, "active-trades"), tradeDocData);
   // listens for changes to doc
-  
-  return {tradeCode: tradeDoc.id}
-}
+
+  return { tradeCode: tradeDoc.id };
+};
 export const deleteActiveTrade = async (tradeCode: string) => {
   // Delete trade from db
   try {
-    await deleteDoc(doc(db, "active-trades", tradeCode))
+    await deleteDoc(doc(db, "active-trades", tradeCode));
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 export const updateActiveTrade = async (tradeCode: string, userData: {}) => {
   try {
     // update the active trade wit the current users information
-    const tradeDoc = doc(db, "active-trades", tradeCode)
-    await updateDoc(tradeDoc, userData)
+    const tradeDoc = doc(db, "active-trades", tradeCode);
+    await updateDoc(tradeDoc, userData);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 export const cancelActiveTrade = async (tradeCode: string) => {
   try {
     // update the active trade wit the current users information
-    const tradeDoc = doc(db, "active-trades", tradeCode)
+    const tradeDoc = doc(db, "active-trades", tradeCode);
     await updateDoc(tradeDoc, {
       isCanceled: true,
-    })
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
+
+export const completeTradeFirebase = async (tradeCode: string) => {
+  try {
+    // update the active trade wit the current users information
+    const userUuid = await _getUuid();
+    const tradeDoc = (
+      await getDoc(doc(db, "active-trades", tradeCode))
+    ).data() as ActiveTradeType;
+    let formattedTradeDoc;
+    if (userUuid === tradeDoc?.receiveUserUuid) {
+      // swap user and receiver
+      formattedTradeDoc = {
+        sendUserUuid: tradeDoc.receiveUserUuid,
+        sendUsername: tradeDoc.receiveUsername,
+        receiveUserUuid: tradeDoc.sendUserUuid,
+        receiveUsername: tradeDoc.sendUsername,
+        sendPinUuid: tradeDoc.receivePinUuid,
+        receivePinUuid: tradeDoc.sendPinUuid,
+        sendPinSrc: tradeDoc.receivePinSrc,
+        receivePinSrc: tradeDoc.sendPinSrc,
+        senderConfirmed: tradeDoc.receiverConfirmed,
+        receiverConfirmed: tradeDoc.senderConfirmed,
+        isCanceled: tradeDoc.isCanceled,
+      };
+    } else {
+      formattedTradeDoc = tradeDoc;
+    }
+
+    // TODO delete the active trade and add the trade to the individule user profiles ********
+    return tradeDoc;
+  } catch (error) {
+    console.error(error);
+  }
+};
--- client/src/screens/main/trading/TradingCompletedScreen.tsx
+++ client/src/screens/main/trading/TradingCompletedScreen.tsx
@@ -1,12 +1,169 @@
-import React, { FC } from 'react'
-import { View } from 'react-native';
+import React, { FC, useEffect, useState } from 'react'
+import { Animated, Easing, View } from 'react-native';
 import styled from 'styled-components/native'
+import { completeTradeFirebase } from '../../../../firebase/FirestoreFunctions';
+import ScreenWrapperComp from '../../../shared/ScreenWrapperComp';
+import { ActiveTradeType } from '../../../../firebase/types/ActiveTradeType';
+import TradingUserTopCard from '../../../components/mainComps/trading/TradingUserTopCard';
+import { BlueGreen, GrandstanderExtraBold, MulishBold, Orange } from '../../../shared/colors';
+import MyCachedImage from '../../../shared/MyCachedImage';
 
-const TradingCompletedScreen: FC = () => {
+const OverallMeatWrapper = styled.View`
+    margin-top: 10%;
+    justify-content: center;
+    align-items: center;
+`
+
+const HeaderText = styled.Text`
+    font-family: ${GrandstanderExtraBold};
+    font-size: 30px;
+    color: ${BlueGreen};
+    margin-bottom: 7%;
+    `
+const SubHeaderCompletedText = styled.Text`
+    font-family: ${MulishBold};
+    font-size: 16px;
+    margin-bottom: 5%;
+`
+const ReceivedText = styled.Text`
+    font-family: ${GrandstanderExtraBold};
+    font-size: 25px;
+    color: ${Orange};
+    margin-top: 10%;
+    margin-bottom: 5%;
+`
+const SendInAndOutWrapper = styled.View`
+    width: 100%;
+    height: 100%;
+`
+
+const PinWrapper = styled.View`
+    width: 50%;
+    height: 30%;
+`
+
+
+const TradingCompletedScreen: FC<any> = ({route, navigation}) => {
+    const {tradeId} = route.params;
+    console.log(tradeId)
+    const [isLoading, setIsLoading] = useState(false);
+    const [tradeData, setTradeData] = useState<ActiveTradeType | null>()
+    const [receiveUserPhoto, setReceiveUserPhoto] = useState<string | null>(null)
+
+    const getTradingData = async () => {
+        setIsLoading(true);
+        const tradeData = (await completeTradeFirebase(tradeId)) as ActiveTradeType 
+        setTradeData(tradeData || null);
+        // TODO get user photo from db
+        const userPhoto = "http://cdn.shopify.com/s/files/1/0238/5301/collections/Teal-Lip.jpg?v=1643402323"
+        setReceiveUserPhoto(userPhoto || null);
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        getTradingData();
+    }, [])
+
+    let ItemsOpacity = new Animated.Value(0.5);
+    let sendInPinY = new Animated.Value(0);
+    let sendOutPinY = new Animated.Value(0);
+
+
+  const sendOutPin = () => {
+    // make an timing animation to move the pin up
+    Animated.timing(sendInPinY, {
+        toValue: -100,
+        duration: 1200,
+        easing: Easing.cubic,
+        useNativeDriver: true,
+    }).start();
+  };
+  const sendInPin = () => {
+    // make an timing animation to move the pin down
+    Animated.timing(sendOutPinY, {
+        toValue: 0,
+        duration: 1200,
+        easing: Easing.cubic,
+        useNativeDriver: true,
+    }).start();
+  }
+  const makeTextVisible = () => {
+    Animated.timing(ItemsOpacity, {
+        toValue: 1,
+        duration: 1200,
+        easing: Easing.cubic,
+        useNativeDriver: true,
+    }).start();
+  }
+
   return (
-    <View>
-      
-    </View>
+    <ScreenWrapperComp>
+        <>
+      {!isLoading && tradeData && receiveUserPhoto && (
+        <>
+        <Animated.View
+            style={{
+                opacity: ItemsOpacity,
+            }}
+            >
+            <OverallMeatWrapper>
+                <HeaderText>Congratulations!!</HeaderText>
+                <SubHeaderCompletedText>You just completed a trade with:</SubHeaderCompletedText>
+                <TradingUserTopCard username={tradeData.receiveUsername} userSrc={receiveUserPhoto} />
+                <ReceivedText>You Received</ReceivedText>
+            </OverallMeatWrapper>
+            <PinWrapper>
+                <MyCachedImage style={{width: "100%", height: "100%"}} resizeMode="contain" src={tradeData.receivePinSrc} />
+            </PinWrapper>
+        </Animated.View>
+
+        {/* send In Pin */}
+        <SendInAndOutWrapper>
+        <Animated.View
+            style={{
+                transform: [
+                    {translateY: sendInPinY}
+                ],
+                width: "100%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+                // position: "absolute",
+                // bottom: "50%",
+                // top: "50%",
+            }}
+        >
+            <PinWrapper>
+                <MyCachedImage style={{width: "100%", height: "100%"}} resizeMode="contain" src={tradeData.receivePinSrc} />
+            </PinWrapper>
+        </Animated.View>
+
+        {/* send Out Pin */}
+        <Animated.View
+            style={{
+                transform: [
+                    {translateY: sendOutPinY}
+                ],
+                width: "100%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+                // position: "absolute",
+                // bottom: "50%",
+                // top: "50%",
+            }}
+        >
+            <PinWrapper>
+                <MyCachedImage style={{width: "100%", height: "100%"}} resizeMode="contain" src={tradeData.sendPinSrc} />
+            </PinWrapper>
+        </Animated.View>
+        </SendInAndOutWrapper>
+        
+        
+        </>
+      )}
+      </>
+    </ScreenWrapperComp>
   )
 }
 
