fb360ea5e32e9d5462ade05f6f11b871104790cf
Justin Albert <Jnalbert879@gmail.com>
2023-02-23 23:47:24 -0800

    updated constants by using extra

--- client/config/firebase.ts
+++ client/config/firebase.ts
@@ -8,14 +8,15 @@ import Constants from "expo-constants";
 
 // Initialize Firebase
 
+
 const firebaseConfig = {
-  apiKey: Constants?.manifest?.extra?.firebaseApiKey,
-  authDomain: Constants?.manifest?.extra?.firebaseAuthDomain,
-  projectId: Constants?.manifest?.extra?.firebaseProjectId,
-  storageBucket: Constants?.manifest?.extra?.firebaseStorageBucket,
-  messagingSenderId: Constants?.manifest?.extra?.firebaseMessagingSenderId,
-  appId: Constants?.manifest?.extra?.firebaseAppId,
-  measurementId: Constants?.manifest?.extra?.firebaseMeasurementId,
+  apiKey: Constants.expoConfig?.extra?.firebaseApiKey,
+  authDomain: Constants.expoConfig?.extra?.firebaseAuthDomain,
+  projectId: Constants.expoConfig?.extra?.firebaseProjectId,
+  storageBucket: Constants.expoConfig?.extra?.firebaseStorageBucket,
+  messagingSenderId: Constants.expoConfig?.extra?.firebaseMessagingSenderId,
+  appId: Constants.expoConfig?.extra?.firebaseAppId,
+  measurementId: Constants.expoConfig?.extra?.firebaseMeasurementId,
 };
 
 const Firebase = initializeApp(firebaseConfig);
