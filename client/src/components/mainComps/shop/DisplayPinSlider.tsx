4653eca4595e3e41becb7db1fabc4fe10e9f7704
Justin Albert <Jnalbert879@gmail.com>
2023-03-04 00:29:02 -0800

    added all pins to the buying slider

--- client/src/components/mainComps/shop/DisplayPinSlider.tsx
+++ client/src/components/mainComps/shop/DisplayPinSlider.tsx
@@ -4,6 +4,8 @@ import styled from "styled-components/native";
 import Carousel from "react-native-reanimated-carousel";
 import { BlueGreen, GrandstanderMedium, GrandstanderSemiBold, Peach, Pink } from "../../../shared/colors";
 import MyCachedImage from '../../../shared/MyCachedImage';
+import { getAllPins } from "../../../../firebase/FirestoreFunctions";
+import { PinTypeDB } from '../../../../firebase/types/PinAndWorldType';
 
 const OverallWrapper = styled.View`
   flex-direction: column;
@@ -45,45 +47,15 @@ const PinLabel = styled.Text`
   color: ${Peach};
 `;
 
-interface PinDataType {
-  src: string;
-  world: string;
-}
-
-const pinsRawData = [
-  {
-    src: "https://firebasestorage.googleapis.com/v0/b/odyssey-28652.appspot.com/o/frank%20traveling.png?alt=media&token=57a5f1c5-9bb9-4a8f-9f16-ff60ef19fdaf",
-    world: "Enchanted Forest",
-  },
-  {
-    src: "https://firebasestorage.googleapis.com/v0/b/odyssey-28652.appspot.com/o/Pang%20in%20a%20flower.png?alt=media&token=5dc21ba1-0c99-4c71-a5f8-ee7e332ea68f",
-    world: "Enchanted Forest",
-  },
-  {
-    src: "https://firebasestorage.googleapis.com/v0/b/odyssey-28652.appspot.com/o/buff%20bunny%20ft.%20sleepy%20shroom.png?alt=media&token=0076d12f-7b39-4e29-9edc-87a3e71b3d95",
-    world: "Enchanted Forest",
-  },
-  {
-    src: "https://firebasestorage.googleapis.com/v0/b/odyssey-28652.appspot.com/o/frankie%20petting%20wolf.png?alt=media&token=7f51fc18-053f-4718-a79b-115bcbf67969",
-    world: "Enchanted Forest",
-  },
-  {
-    src: "https://firebasestorage.googleapis.com/v0/b/odyssey-28652.appspot.com/o/pang%20with%20leaf%20umbrella.png?alt=media&token=bafd86cc-0223-4e52-bcdf-a9e7a0ce1492",
-    world: "Enchanted Forest",
-  },
-  {
-    src: "https://firebasestorage.googleapis.com/v0/b/odyssey-28652.appspot.com/o/Pang%20searching%20for%20home%20(2).png?alt=media&token=8f976977-5764-4061-9425-71a52f644c73",
-    world: "Deep Sea",
-  },
-];
 
 const DisplayPinSlider: FC = () => {
-  const [pinsData, setPinsData] = useState<PinDataType[]>([]);
+  const [pinsData, setPinsData] = useState<PinTypeDB[]>([]);
   const [currentLabel, setCurrentLabel] = useState<string>("");
 
   const getPinsData = async () => {
     // fetch pins from firebase server
-    setPinsData(pinsRawData);
+    const allPinsData = await getAllPins()
+    setPinsData(allPinsData || []);
   };
   useEffect(() => {
     getPinsData();
@@ -106,7 +78,7 @@ const DisplayPinSlider: FC = () => {
           scrollAnimationDuration={300}
           overscrollEnabled={true}
           onSnapToItem={(index) => {
-            handlePinLabel(pinsData[index].world);
+            handlePinLabel(pinsData[index].worldName);
           }}
           renderItem={(pin) => (
             <PinImageWrapper>
