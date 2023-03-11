511e6f4fda096e66f475428b8a0889ac2d52015d
Justin Albert <Jnalbert879@gmail.com>
2023-03-10 23:14:28 -0800

    changed you know what

--- bloomberg-terminal/src/utils/firebase.ts
+++ bloomberg-terminal/src/utils/firebase.ts
@@ -14,13 +14,13 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_BLAHHBLAHH1,
+  authDomain: process.env.REACT_APP_BLAHHHLAHH2,
+  projectId: process.env.REACT_APP_BLAHHHLAHH3,
+  storageBucket: process.env.REACT_APP_BLAHHHLAHH4,
+  messagingSenderId: process.env.REACT_APP_BLAHHHLAHH5,
+  appId: process.env.REACT_APP_BLAHHHLAHH6,
+  measurementId: process.env.REACT_APP_BLAHHHLAHH7,
 };
 // Initialize Firebase
 
