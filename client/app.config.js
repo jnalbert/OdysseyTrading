dc1eb25de5fbfcac5f8eda8d03a948201ea1b20d
Justin Albert <Jnalbert879@gmail.com>
2023-03-12 15:32:06 -0700

    ready for build 1.0.1(6)

--- client/app.config.js
+++ client/app.config.js
@@ -20,7 +20,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.jnalbert879.odysseytrading",
-      buildNumber: "5",
+      buildNumber: "6",
     },
     android: {
       adaptiveIcon: {
